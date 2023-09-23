b491a2a723409aed2c5eac839dbd7f8596a905ac
comflife <okharry1@naver.com>
2023-09-23 10:20:42 +0900

    complex steer

--- src/steadylab/steadylab/missions/complex_area.py
+++ src/steadylab/steadylab/missions/complex_area.py
@@ -41,12 +41,9 @@ from missions.mission import Mission
 class ComplexArea(Mission):
     def __init__(self, queue_size: int = 10,min_duration: float = 2.0,qos: int = 5):
         super().__init__("complex_area", queue_size, min_duration, qos)
-        self.objects = [24, 25, 26, 27, 28, 29]
-        self.erp = WriteCar()
-        # self.box = BoundingBox()
         
-        self._publishers = {"steer": self.create_publisher(Int64, "/dynamic_obstacle_steer", qos),
-                            "running": self.create_publisher(Bool, "/dynamic_obstacle", qos)}
+        self._publishers = {"steer": self.create_publisher(Int64, "/complex_steer", qos),
+                            "running": self.create_publisher(Bool, "/complex_area", qos)}
         self._subscribers = {"yolo": self.create_subscription(BoundingBoxes, "/yolo", self.yolo_callback, qos)}
     
         self.timer = self.create_timer(0.1, self.callback)
@@ -59,18 +56,82 @@ class ComplexArea(Mission):
         self._publishers["running"].publish(running)
     
     
-    def yolo_callback(self, msg: BoundingBoxes):
+    def yolo_callback(self, data):
         self.reset()
-        
-    #     for box in msg.boxes:
-    #         _ = self.queue.popleft()
-    #         self.queue.append(box.cls)
-            
-    #     counts = sum([self.queue.count(o) for o in self.objects])
-        
-    #     if counts >= int(0.8*len(self.queue)):
-    #         self.running = True
-    #         self.last = time.time()
+        box_cls = data.boxes[0]
+        box_name = data.boxes[1]
+        box_xn = data.boxes[4][0]
+        # boxes[4] = xywhn, boxes[4][0] = normalized x(0~1)
+        box_conf = data.boxes[2]
+
+        if 18 == box_cls and box_conf > 0.3:
+            print("x_arrow")
+            if (0.5-0.01) <= box_xn < (0.5-0.005):
+                self.steer = -60
+                print("turn left, steer -60")
+            elif (0.5-0.4) <= box_xn < (0.5-0.01):
+                self.steer = -90
+                print("turn left, steer -90")
+            elif (0.5+0.005) <= box_xn < (0.5+0.01):
+                self.steer = 60
+                print("turn right, steer 60")
+            elif (0.5+0.01) <= box_xn < (0.5+0.04):
+                self.steer = 90
+                print("turn right, steer 90")
+
+        elif 3 == box_cls and box_conf > 0.3:
+            print(box_name) #left_arrow
+            if (0.5-0.01) <= box_xn < (0.5-0.005):
+                self.steer = -60
+                print("turn left, steer -60")
+            elif (0.5-0.4) <= box_xn < (0.5-0.01):
+                self.steer = -90
+                print("turn left, steer -90")
+            elif (0.5+0.005) <= box_xn < (0.5+0.01):
+                self.steer = 60
+                print("turn right, steer 60")
+            elif (0.5+0.01) <= box_xn < (0.5+0.04):
+                self.steer = 90
+                print("turn right, steer 90")
+
+        elif 11 == box_cls and box_conf > 0.3:
+            print(box_name) #stop_line
+            if (0.5-0.01) <= box_xn < (0.5-0.005):
+                self.steer = -60
+                print("turn left, steer -60")
+            elif (0.5-0.4) <= box_xn < (0.5-0.01):
+                self.steer = -90
+                print("turn left, steer -90")
+            elif (0.5+0.005) <= box_xn < (0.5+0.01):
+                self.steer = 60
+                print("turn right, steer 60")
+            elif (0.5+0.01) <= box_xn < (0.5+0.04):
+                self.steer = 90
+                print("turn right, steer 90")
+
+        elif 9 == box_cls and box_conf > 0.3:
+            print(box_name) #speed_bump
+            if (0.5-0.01) <= box_xn < (0.5-0.005):
+                self.steer = -60
+                print("turn left, steer -60")
+            elif (0.5-0.4) <= box_xn < (0.5-0.01):
+                self.steer = -90
+                print("turn left, steer -90")
+            elif (0.5+0.005) <= box_xn < (0.5+0.01):
+                self.steer = 60
+                print("turn right, steer 60")
+            elif (0.5+0.01) <= box_xn < (0.5+0.04):
+                self.steer = 90
+                print("turn right, steer 90")
+
+        elif 8 == box_cls and box_conf > 0.3:
+            print(box_name) #slash
+            self.steer = 0
+
+        elif 10 == box_cls and box_conf > 0.3:
+            print(box_name) #speed_sign
+            self.steer = 0
+
     
     
 def main(args=None):
